d814701549254c0109179b2a60984751c75ff586
VuDangKhoa <dangkhoa09102004@gmail.com>
2025-05-23 15:10:10 +0700

    update route_web v2

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CampaignController;
-use App\Http\Controllers\DonationController;
-use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\NewsController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaigns.index');
+Route::get('/campaigns/{id}', [CampaignController::class, 'show'])->name('campaigns.show');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/news/{id}', [NewsController::class, 'show'])->name('news.show');
 
-// Campaign routes
-Route::resource('campaigns', CampaignController::class);
-Route::get('campaigns/{campaign}/donate', [CampaignController::class, 'donate'])->name('campaigns.donate');
-
-// Donation routes
-Route::resource('donations', DonationController::class);
-Route::post('donations/process', [DonationController::class, 'process'])->name('donations.process');
-
-// Activity routes
-Route::resource('activities', ActivityController::class);
-
-// News routes
-Route::resource('news', NewsController::class);
-
-// Admin routes
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin', function () {
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
         return view('admin.dashboard');
-    })->name('admin.dashboard');
-    
-    Route::get('/admin/campaigns', [CampaignController::class, 'adminIndex'])->name('admin.campaigns.index');
-    Route::get('/admin/donations', [DonationController::class, 'adminIndex'])->name('admin.donations.index');
-    Route::get('/admin/activities', [ActivityController::class, 'adminIndex'])->name('admin.activities.index');
-    Route::get('/admin/news', [NewsController::class, 'adminIndex'])->name('admin.news.index');
+    })->name('dashboard');
 });
