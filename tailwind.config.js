4b67417e9363c6e3a657be891f254779d6263a9a
VuDangKhoa <dangkhoa09102004@gmail.com>
2025-05-23 15:06:06 +0700

    update lib_inter v1

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          50: '#fff8f1',
+          100: '#feecd3',
+          500: '#f97316', // orange-500
+          600: '#ea580c', // orange-600
+        },
+        red: {
+          400: '#f87171',
+          600: '#dc2626',
+        },
+        yellow: {
+          300: '#fcd34d',
+          400: '#fbbf24',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
